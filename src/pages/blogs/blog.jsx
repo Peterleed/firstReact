7de558af89bb7bba8b0eee8795d5f7c9dd4e4f2a
peter <peteralabi73@gmail.com>
2024-10-17 17:46:35 +0100

    added pagination for the blog pages

--- src/pages/blogs/blog.jsx
+++ src/pages/blogs/blog.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import Card from "../../comps/blog/card";
 import { blogsData } from "../../utils/data";
 import styles from "../blogs/blog.module.css";
 
 const Blog = () => {
+  const [blogsPerPage, setBlogsPerPage] = useState(3);
+  const currentBlogs = blogsData.slice(0, blogsPerPage);
+
+  const hide = currentBlogs.length === blogsData.length;
+
   return (
-    <div className={styles.blogPage}>
-      {blogsData.map((blog, i) => (
-        <Card blog={blog} key={i} />
-      ))}
+    <div className={styles.blogs}>
+      <div className={styles.blogPage}>
+        {currentBlogs.map((blog, i) => (
+          <Card blog={blog} key={i} />
+        ))}
+      </div>
+
+      <div
+        className={hide ? `${styles.hidden}` : `${styles.loadMore}`}
+        onClick={() => setBlogsPerPage(blogsPerPage + 3)}
+      >
+        <p>Load More</p>
+      </div>
     </div>
   );
 };
